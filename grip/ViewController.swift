78c9b847d97320da1d307adf42fdf76c6fa60124
leesam <kofsiwon@siwonschool.com>
2021-04-29 23:29:45 +0900

    Initial Commit

---
+++ grip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  grip
+//
+//  Created by 영준 이 on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
